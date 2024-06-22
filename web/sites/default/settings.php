c2643c7891c304d8425b5a717129b7d9aaafd0e1
Rodrigo <roagrodriguez@gmail.com>
2024-06-22 19:11:07 +0200

    Enable status_cache

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -865,3 +865,8 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+/**
+ * Custom settings.
+ */
+$settings['state_cache'] = TRUE;
